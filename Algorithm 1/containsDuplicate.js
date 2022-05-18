737c849a74fb8e83b1cc23414a7615ae58d3aae7
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-05-18 11:01:28 -0400

    finished 217

---
+++ Algorithm 1/containsDuplicate.js
@@ -0,0 +1,18 @@
+/**
+ * 217. Contains Duplicate
+ * 
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ * 
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+    nums.sort();
+    let k = 0;
+    while(k<nums.length){
+        if(nums[k]==nums[k+1]){
+            return true;
+        }
+        k++
+    } return false;
+};
