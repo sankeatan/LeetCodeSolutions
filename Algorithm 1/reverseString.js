a659db89ed62f00cf174a8287fa7de327ffcc8d6
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-05-19 10:38:46 -0400

    finished 344

---
+++ Algorithm 1/reverseString.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+ * 
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+    s.reverse();
+};
