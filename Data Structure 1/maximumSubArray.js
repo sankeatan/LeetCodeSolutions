996945c9e8bbb4c1bc1aebbc9bfe38c9f5cef931
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-05-19 11:05:19 -0400

    53 finished

---
+++ Data Structure 1/maximumSubArray.js
@@ -0,0 +1,22 @@
+/**
+ * 53. Maximum Subarray 
+ * 
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+    let largest = -Infinity;
+    let potential = nums[0];
+    let k = 1;
+    if(nums.length == 1){
+        return nums[0];
+    }
+    while (k<nums.length){
+        potential=Math.max(potential+nums[k], nums[k]);
+        largest=Math.max(largest, potential);
+        k++;
+        }
+    return largest;
+}
