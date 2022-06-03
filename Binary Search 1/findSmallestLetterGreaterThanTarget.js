5123867d96d7f622f138b65382386b4b291d2cb5
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-06-03 06:57:55 -0400

    solved 744

---
+++ Binary Search 1/findSmallestLetterGreaterThanTarget.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target.
+ * 
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+ var nextGreatestLetter = function(letters, target) {
+    let i = 0;
+    while (i<=letters.length-1){
+        if(letters[i]>target){
+            return letters[i];
+        } i++;
+    } return letters[0];
+};
